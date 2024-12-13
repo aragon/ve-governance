b095eb0107459824386156e29a54e4f903e22db2
xavikh <xavikh@gmail.com>
2024-12-13 15:09:48 +0100

    Reduce Lock gap

--- src/escrow/increasing/Lock.sol
+++ src/escrow/increasing/Lock.sol
@@ -168,5 +168,5 @@ contract Lock is
     /// @notice Internal method authorizing the upgrade of the contract via the [upgradeability mechanism for UUPS proxies](https://docs.openzeppelin.com/contracts/4.x/api/proxy#UUPSUpgradeable) (see [ERC-1822](https://eips.ethereum.org/EIPS/eip-1822)).
     function _authorizeUpgrade(address) internal virtual override auth(LOCK_ADMIN_ROLE) {}
 
-    uint256[48] private __gap;
+    uint256[47] private __gap;
 }
