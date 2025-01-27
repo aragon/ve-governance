1cdecaece3a3c853a67a4c391b71b552f1a87300
jordaniza <j@jordaniza.com>
2025-01-27 21:30:28 +0400

    rd-1105: redeploy with upgradeable token

--- test/mocks/MockERC20Upgradeable.sol
+++ test/mocks/MockERC20Upgradeable.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-
-contract MockERC20Upgradeable is ERC20Upgradeable {
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+contract MockERC20Upgradeable is ERC20Upgradeable, UUPSUpgradeable {
     function initialize() external initializer {
         __ERC20_init("TestToken", "TST");
     }
@@ -11,4 +11,7 @@ contract MockERC20Upgradeable is ERC20Upgradeable {
     function mint(address to, uint256 amount) external {
         _mint(to, amount);
     }
+
+    // open upgrade
+    function _authorizeUpgrade(address newImplementation) internal override {}
 }
