ec18adae55d996d2891c4db42111e0e2fb887ce3
xavikh <xavikh@gmail.com>
2025-02-10 13:09:31 +0100

    Fix testUpgrades base version

--- test/upgrades/TestUpgrades.t.sol
+++ test/upgrades/TestUpgrades.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.17;
 
 import {Upgrades} from "openzeppelin-foundry-upgrades/LegacyUpgrades.sol";
 import {Test} from "forge-std/Test.sol";
-//import {Clock} from '@aragon/ve-governance-v100/clock/Clock.sol';
+//import {Clock} from '@aragon/ve-governance-v101/clock/Clock.sol';
 //import {Clock as ClockV2} from '@clock/Clock.sol';
 
 import { Options } from "openzeppelin-foundry-upgrades/Options.sol";
@@ -13,8 +13,8 @@ contract AragonTest is Test {
         //Clock clock = new Clock();
 
         Options memory options;
-        options.referenceBuildInfoDir = "ref_builds/build-info-v100";
-        options.referenceContract = "build-info-v100:Clock";
+        options.referenceBuildInfoDir = "ref_builds/build-info-v101";
+        options.referenceContract = "build-info-v101:Clock";
         
         Upgrades.validateUpgrade("Clock.sol", options);
     }
