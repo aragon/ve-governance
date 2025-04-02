4ddf9e05edb7adfd2354c886156b3feb3a0c301e
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2025-04-02 12:48:14 +0400

    comment for canmerge

--- src/escrow/VotingEscrowIncreasing_v1_4_0.sol
+++ src/escrow/VotingEscrowIncreasing_v1_4_0.sol
@@ -346,13 +346,13 @@ contract VotingEscrowV1_4_0 is
     ) public view returns (bool) {
         uint256 maxTime = IEscrowCurve(curve).maxTime();
 
-        uint48 oldLockedFromEnd = uint48(_fromLocked.start + maxTime);
-        uint48 oldLockedToEnd = uint48(_toLocked.start + maxTime);
+        uint256 fromLockedEnd = _fromLocked.start + maxTime;
+        uint256 toLockedEnd = _toLocked.start + maxTime;
 
+        // Tokens either must have the same start dates or both must be mature.
         if (
             (_toLocked.start != _fromLocked.start) &&
-            // TODO: GIORGI <= sign or < ?
-            (block.timestamp <= oldLockedToEnd || block.timestamp <= oldLockedFromEnd)
+            (toLockedEnd >= block.timestamp || fromLockedEnd >= block.timestamp)
         ) {
             return false;
         }
