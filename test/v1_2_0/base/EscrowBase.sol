2de06822e6fb7886af7da301d8bf6b6cffc3451a
jordaniza <j@jordaniza.com>
2025-04-11 11:27:06 +0400

    restored removal of the globalPoint

--- test/v1_2_0/base/EscrowBase.sol
+++ test/v1_2_0/base/EscrowBase.sol
@@ -100,22 +100,12 @@ contract EscrowBase is
         escrow = _deployEscrow(address(token), address(dao), address(clock), 1);
         curve = _deployCurve(address(escrow), address(dao), warmupPeriod, address(clock));
         nftLock = _deployLock(address(escrow), name, symbol, address(dao));
-        ivotesAdapter = _deployEscrowIVotesAdapter(
-            address(dao),
-            address(escrow),
-            address(clock)
-        );
+        ivotesAdapter = _deployEscrowIVotesAdapter(address(dao), address(escrow), address(clock));
 
         super.initialize(curve.maxTime(), clock.checkpointInterval());
 
         // to be added as proxies
-        voter = _deployVoter(
-            address(dao),
-            address(escrow),
-            false,
-            address(clock),
-            address(ivotesAdapter)
-        );
+        voter = _deployVoter(address(dao), address(escrow), false, address(clock));
         queue = _deployExitQueue(address(escrow), 3 days, address(dao), 0, address(clock), 1);
 
         // grant this contract admin privileges
@@ -198,24 +188,6 @@ contract EscrowBase is
         assertEq(tokenP.coefficients[1], _slopeFP);
         assertEq(tokenP.checkpointTs, _checkpointTs);
         assertEq(tokenP.writtenTs, _writtenTs);
-    }
-
-    function assertGlobalPoint(
-        uint256 _expectedLatestIndex,
-        int256 _biasFP,
-        int256 _slopeFP,
-        uint256 _writtenTs
-    ) internal view {
-        uint256 latestIndex = curve.globalPointLatestIndex();
-        assertEq(latestIndex, _expectedLatestIndex);
-        GlobalPoint memory p = curve.globalPointHistory(latestIndex);
-        assertEq(p.writtenTs, _writtenTs);
-        assertEq(p.bias, _biasFP);
-        assertEq(p.slope, _slopeFP);
-    }
-
-    function assertTotalSupply(uint256 _t, int256 _amountFP) internal view {
-        assertEq(curve.supplyAt(_t), uint256(_amountFP / 1e18));
     }
 
     function assertVotingPower(uint256 _tokenId, int256 _amountFP) internal view {
@@ -314,20 +286,17 @@ contract EscrowBase is
         return LinearIncreasingEscrow(address(impl).deployUUPSProxy(initCalldata));
     }
 
-    
-
     function _deployVoter(
         address _dao,
         address _escrow,
         bool _reset,
-        address _clock,
-        address _ivotesAdapter
+        address _clock
     ) public returns (SimpleGaugeVoter) {
         SimpleGaugeVoter impl = new SimpleGaugeVoter();
 
         bytes memory initCalldata = abi.encodeCall(
             SimpleGaugeVoter.initialize,
-            (_dao, _escrow, _reset, _clock, _ivotesAdapter, true)
+            (_dao, _escrow, _reset, _clock)
         );
         return SimpleGaugeVoter(address(impl).deployUUPSProxy(initCalldata));
     }
