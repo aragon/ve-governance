fa60bb92bcd1baa91c943f9950380a9a010f25b9
xavikh <xavikh@gmail.com>
2025-05-09 10:41:44 +0200

    Update CurveConstantLib comments

--- src/libs/CurveConstantLib.sol
+++ src/libs/CurveConstantLib.sol
@@ -6,9 +6,9 @@ pragma solidity ^0.8.0;
 /// @dev The coefficients are precomputed for the curve: they represent a linear increase of 3x over 6 weeks
 library CurveConstantLib {
     int256 internal constant SHARED_CONSTANT_COEFFICIENT = 1e18;
-    /// @dev 2 / (7 * 2_weeks) - expressed in fixed point
+    /// @dev (2e18) / (3 epochs * 3600*24*7*2) - expressed in fixed point
     int256 internal constant SHARED_LINEAR_COEFFICIENT = 551146384479;
-    /// @dev 1 / (7 * (2_weeks)^2) - expressed in fixed point
+    /// @dev 0 - since the curve is linear, the quadratic coefficient is 0
     int256 internal constant SHARED_QUADRATIC_COEFFICIENT = 0;
 
     /// @dev the maxiumum number of epochs the cure can keep increasing
