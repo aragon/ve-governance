738ac7690f2166241b9897f114ac9b1752e785ab
Giorgi Lagidze <giorgilagidze0920@gmail.com>
2025-08-27 23:54:58 +0400

    uncomment

--- test/v1_2_0/invariant/Delegation.t.sol
+++ test/v1_2_0/invariant/Delegation.t.sol
@@ -86,32 +86,26 @@ contract TestDelegationInvariant is IEscrowCurveTokenStorage, EscrowBase {
         assertEq(MockERC20(escrow.token()).balanceOf(address(escrow)), escrow.totalLocked());
     }
 
-    // TODO fails as supply is not implemented yet
-    // function invariant_SumOfNftsAmountsEqualTotalLocked() public {
-    //     uint256 amountSum = 0;
-    //     uint256 vpSum = 0;
-    //
-    //     uint256 globalPower = escrow.totalVotingPower();
-    //
-    //     uint256[] memory ids = h.getActiveTokenIds();
-    //     for (uint256 i = 0; i < ids.length; i++) {
-    //         uint256 tokenId = ids[i];
-    //
-    //         uint256 vp = escrow.votingPower(tokenId);
-    //         uint256 amount = escrow.locked(tokenId).amount;
-    //
-    //         amountSum += amount;
-    //         vpSum += vp;
-    //     }
-    //
-    //     assertEq(amountSum, escrow.totalLocked(), "Sum of NFT Amoutns != totalLocked");
-    //     assertApproxEqAbs(
-    //         vpSum,
-    //         escrow.totalVotingPower(),
-    //         ids.length,
-    //         "Sum of vps individually != total vp"
-    //     );
-    // }
+    // Note that this doesn't use `totalVotingPower` checks as 
+    // the escrow of 1_2_0 uses curve with no supply
+    // (i.e doesn't implement total supply).
+    function invariant_SumOfNftsAmountsEqualTotalLocked() public {
+        uint256 amountSum = 0;
+        uint256 vpSum = 0;
+    
+        uint256[] memory ids = h.getActiveTokenIds();
+        for (uint256 i = 0; i < ids.length; i++) {
+            uint256 tokenId = ids[i];
+    
+            uint256 vp = escrow.votingPower(tokenId);
+            uint256 amount = escrow.locked(tokenId).amount;
+    
+            amountSum += amount;
+            vpSum += vp;
+        }
+    
+        assertEq(amountSum, escrow.totalLocked(), "Sum of NFT Amoutns != totalLocked");
+    }
 
     function invariant_UserHasCorrectPastVotes() public {
         address[] memory actors = h.getActors();
@@ -158,8 +152,4 @@ contract TestDelegationInvariant is IEscrowCurveTokenStorage, EscrowBase {
             assertApproxEqAbs(totalOnGaugeVoter, totalPastVotes, delta);
         }
     }
-    // TODO fails as totalSupply is not implemented yet
-    // function invariant_TotalVotingPowerDoesNotExceedTotalLocked() public {
-    //     assertLe(escrow.totalVotingPower(), bias(h.totalLocked(), maxTime));
-    // }
 }
