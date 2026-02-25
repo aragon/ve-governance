d7156c06b00fe52d61ebaa9041d614470d29b678
jordaniza <j@jordaniza.com>
2026-02-25 12:49:05 +0400

    revert comment change

--- src/libs/CurveConstantLib.sol
+++ src/libs/CurveConstantLib.sol
@@ -22,7 +22,7 @@ library CurveConstantLib {
     int256 internal constant SHARED_QUADRATIC_COEFFICIENT = 0;
 
     /// @dev the maxiumum number of epochs the cure can keep increasing
-    /// 1 year / 2 weeks
+    /// 26 epochs in a year, 2 years = 52 epochs
     uint256 internal constant MAX_EPOCHS = 52;
 
     function getCoefficients() internal pure returns (int256[3] memory, uint256) {
